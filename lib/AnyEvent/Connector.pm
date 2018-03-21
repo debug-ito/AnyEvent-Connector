d73c6f47495970f4db275882b9f1e893e24ae823
Toshio Ito <debug.ito@gmail.com>
2018-03-21 20:55:40 +0900

    writing doc.

--- lib/AnyEvent/Connector.pm
+++ lib/AnyEvent/Connector.pm
@@ -11,13 +11,115 @@ __END__
 
 =head1 NAME
 
-AnyEvent::Connector - abstract
+AnyEvent::Connector - tcp_connect with transparent proxy handling
 
 =head1 SYNOPSIS
 
+    use AnyEvent::Connector;
+    
+    ## Specify the proxy setting explicitly.
+    my $c = AnyEvent::Connector->new(
+        proxy => 'http://proxy.example.com:8080',
+        no_proxy => ['localhost', 'your-internal-domain.net']
+    );
+    
+    ## Proxy setting from "http_proxy" and "no_proxy" environment variables.
+    my $cenv = AnyEvent::Connector->new(
+        env_proxy => "http",
+    );
+    
+    ## Same API as AnyEvent::Socket::tcp_connect
+    my $guard = $c->tcp_connect(
+        "target.hogehoge.org", 80,
+        sub {
+            ## connect callback
+            my ($fh ,$host, $port, $retry) = @_;
+            ...;
+        },
+        sub {
+            ## prepare calback
+            my ($fh) = @_;
+            ...;
+        }
+    );
+
 =head1 DESCRIPTION
 
+L<AnyEvent::Connector> object has C<tcp_connect> method compatible
+with that from L<AnyEvent::Socket>, and it handles proxy settings
+transparently.
+
+=head1 CLASS METHODS
+
+=head2 $conn = AnyEvent::Connector->new(%args)
+
+The constructor.
+
+Fields in C<%args> are:
+
+=over
+
+=item C<proxy> => STR (optional)
+
+String of proxy URL. Currently only C<http> proxy is supported.
+
+If both C<proxy> and C<env_proxy> are not specified, the C<$conn> will directly connect to the destination host.
+
+If both C<proxy> and C<env_proxy> are specified, setting by C<proxy> is used.
+
+Setting empty string to C<proxy> disables the proxy setting done by C<env_proxy> option.
+
+=item C<no_proxy> => STR or ARRAYREF of STR (optional)
+
+String or array-ref of strings of domain names, to which the C<$conn> will directly connect.
+
+If both C<no_proxy> and C<env_proxy> are specified, setting by C<no_proxy> is used.
+
+Setting empty string or empty array-ref to C<no_proxy> disables the no_proxy setting done by C<env_proxy> option.
+
+=item C<env_proxy> => STR (optional)
+
+String of protocol specifier. If specified, proxy settings for that
+protocol are loaded from environment variables, and C<$conn> is
+created.
+
+For example, if C<"http"> is specified, C<http_proxy> (or
+C<HTTP_PROXY>) and C<no_proxy> (or C<NO_PROXY>) environment variables
+are used to set C<proxy> and C<no_proxy> options.
+
+C<proxy> and C<no_proxy> options have precedence over C<env_proxy>
+option.
+
+=back
+
+=head1 OBJECT METHOD
+
+=head2 $guard = $conn->tcp_connect($host, $port, $connect_cb, $prepare_cb)
+
+Make a TCP connection to the given C<$host> and C<$port>.
+
+
+=head2 $proxy = $conn->proxy_for($host, $port)
+
+If a proxy is used for connecting to the given C<$host> and C<$port>,
+it returns the string of the proxy URL. Otherwise, it returns
+C<undef>.
+
+
 =head1 SEE ALSO
+
+=over
+
+=item *
+
+L<AnyEvent::Socket>
+
+=item *
+
+L<AnyEvent::HTTP> - it has C<tcp_connect> option to implement proxy
+connection. You can use L<AnyEvent::Connector> for it.
+
+=back
 
 =head1 REPOSITORY
 
